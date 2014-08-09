e70db3152990221e53a37096de4f7308947b8db3
Kien Nguyen <kiennt.sic@gmail.com>
2014-08-09 00:21:58 -0500

    add some more unit test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -8,5 +10,47 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:description].any?
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title", description: "hahah", image_url: "hoho.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "MyBookTitle",
+                description: "yyy",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "Image url" do
+    ok = %w{fred.gif fred.jpg FRED.png}
+    bad = %w{fred.doc fred.gif.more}
+    ok.each do |name|
+      assert new_product(name).valid? "#{name} should be valid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid? "#{name} should be invalid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
   end
 end
